1e84dcd7ce2863dbd7678426e3eaaf8e6ac25064
amgadelmarghny <amgadsaad22@gmail.com>
2024-01-25 10:36:37 +0200

    create theme style

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/shared/cubit/app_cupit/app_cubit.dart';
 import 'package:news_app/shared/cubit/my_observe.dart';
 import 'package:news_app/shared/network/local/shared_pref_helper.dart';
 import 'package:news_app/shared/network/remot/dio_helper.dart';
+import 'package:news_app/shared/style/theme.dart';
 import 'package:news_app/views/home_view.dart';
 
 void main() async {
@@ -35,48 +35,8 @@ class _NewsAppState extends State<NewsApp> {
         builder: (context, state) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              primarySwatch: Colors.teal,
-              brightness: Brightness.light,
-              textTheme: const TextTheme(
-                bodyLarge: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                ),
-              ),
-              appBarTheme: const AppBarTheme(
-                titleSpacing: 20,
-                iconTheme: IconThemeData(size: 30),
-                backgroundColor: Colors.white,
-                systemOverlayStyle: SystemUiOverlayStyle(
-                  statusBarIconBrightness: Brightness.dark,
-                  statusBarColor: Colors.white,
-                ),
-              ),
-              bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-                selectedItemColor: Colors.teal,
-                elevation: 20,
-              ),
-            ),
-            darkTheme: ThemeData(
-              brightness: Brightness.dark,
-              primarySwatch: Colors.teal,
-              appBarTheme: const AppBarTheme(
-                titleSpacing: 20,
-                iconTheme: IconThemeData(size: 30),
-              ),
-              textTheme: const TextTheme(
-                bodyLarge: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white,
-                ),
-              ),
-              bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-                selectedItemColor: Colors.tealAccent,
-              ),
-            ),
+            theme: ThemeStyle.lightTheme(),
+            darkTheme: ThemeStyle.darkTheme(),
             themeMode: BlocProvider.of<AppCubit>(context).isDark
                 ? ThemeMode.dark
                 : ThemeMode.light,
